1f40333de0a80a6e127893531859a5da9ee99dba
Pablo Brito <pablo_pbc@outlook.com>
2023-05-13 12:30:59 -0300

    Screen: hello screen OK - missing backend yet

--- app/src/main/java/com/example/connectfood/LoginActivity.kt
+++ app/src/main/java/com/example/connectfood/LoginActivity.kt
@@ -13,47 +13,49 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        // Variáveis de exemplo de login e senha válidas
+        // User's login and password examples
         val validLogin = "01123456000110"
-        val validPassword = "password123"
+        val validPassword = "admin123"
 
-        //Identificando os campos de login e senha
+        //Identifying login and password fields
         val loginInput = findViewById<EditText>(R.id.signInLoginInput)
         val passwordInput = findViewById<EditText>(R.id.signInPasswordInput)
 
-        //Pegando os valores informados pelo usuário
-        val inputedlogin = loginInput.text.toString()
-        val password = passwordInput.text.toString()
 
-        // Formatando o login do usuario 01.123.456/0001-10 -> 01123456000110
-        val formatedLogin = inputedlogin.replace("[^0-9]".toRegex(), "")
 
-        // Função para validação do login do usuário, aqui tem que usar algum método de autentificação
+        // Function to validate the user's login, here we have to use some auth method
         fun validateLogin(login: String, password: String): Boolean {
             return login == validLogin && password == validPassword
         }
 
-        // Função para realizar a validação e exibir mensagem de erro caso necessário
+        // Function to realize the validation and show the error message if needed
         fun signIn() {
+            //Getting the data informed by user
+            val inputedlogin = loginInput.text.toString()
+            val password = passwordInput.text.toString()
+
+            // Formatting the user login 01.123.456/0001-10 -> 01123456000110
+            val formatedLogin = inputedlogin.replace("[^0-9]".toRegex(), "")
+
             if (validateLogin(formatedLogin, password)) {
-                // Caso as informações de login sejam válidas, realizar a ação desejada
+                // In case of correct login and password
                 Toast.makeText(this, "Login realizado com sucesso!", Toast.LENGTH_SHORT).show()
 
-                // Indo para a próxima tela
+                // Going to the next screen
                 val helloScreen = Intent(this, DonorRecipientsHelloScrActivity::class.java)
                 helloScreen.putExtra("login", formatedLogin)
                 startActivity(helloScreen)
             } else {
-                // Caso as informações de login sejam inválidas, exibir mensagem de erro
+                // In case of incorrect login and password
                 Toast.makeText(this, "Login ou senha incorreto!", Toast.LENGTH_SHORT).show()
             }
         }
 
-        // Configuração do botão de login para chamar a função signIn()
+        // Calling the function signIn by clicking on the button signInBtn
         val signInBtn = findViewById<Button>(R.id.signInBtn)
         signInBtn.setOnClickListener { signIn() }
 
-        // Configuração do botão de cadastro para ir para a tela de cadastro
+        // Navigating to the Register screen by clicking on register button
         val signUpBtn = findViewById<TextView>(R.id.signInSignUpBtn)
         signUpBtn.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
